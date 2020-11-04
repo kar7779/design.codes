06e8ca48449dfd207f6d1b624e5b0236cab14a11
karthik <karthikchinu777@gmail.com>
2020-11-04 19:32:32 +0530

    added hamburg-menu

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,34 +1,39 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { Link } from "react-router-dom";
 import { Switch } from "react-router-dom/cjs/react-router-dom.min";
 import M from "materialize-css";
-export default class nav extends Component {
-  componentDidMount() {
-    M.AutoInit();
-    M.Sidenav.init(this.sidenav);
-  }
-  render() {
-    return (
-      <div>
-        <nav>
-          <Switch>
-            <div className="nav-wrapper">
-              <h1>Designcodes</h1>
-              <ul>
-                <li>
-                  <Link to={"/home"}>Home</Link>
-                </li>
-                <li>
-                  <Link to={"/about"}>about</Link>
-                </li>
-                <li>
-                  <Link to={"/contact"}>conatct</Link>
-                </li>
-              </ul>
-            </div>
-          </Switch>
-        </nav>
-      </div>
-    );
-  }
-}
+import Sidebar from "./sidebar";
+
+const Nav = () => {
+  const [open, setopen] = useState(false);
+  return (
+    <div>
+      <nav>
+        <Switch>
+          <div className="nav-wrapper">
+            <h1>Designcodes</h1>
+            <ul className={open ? "navmenu-active" : "sidebar"}>
+              <li>
+                <Link to={"/home"}>Home</Link>
+              </li>
+              <li>
+                <Link to={"/about"}>about</Link>
+              </li>
+              <li>
+                <Link to={"/contact"}>conatct</Link>
+              </li>
+            </ul>
+          </div>
+        </Switch>
+        <div className="hamburgmenu" onClick={() => setopen(!open)}>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+      </nav>
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Nav;
---
+++ src/components/nav/sidebar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <a href="">Home</a>
+      <a href="">Contact</a>
+      <a href="">about</a>
+    </div>
+  );
+}
+
+export default Sidebar;
